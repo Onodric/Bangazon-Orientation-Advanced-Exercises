d94caa1aa03663cf49176eb9d7c0f5fa9aac41cf
Ben Marks <onodric@yahoo.com>
2017-01-05 23:26:58 -0600

    ex 2 comprehension done

--- 02-comprehension.py
+++ 02-comprehension.py
@@ -1,38 +1,25 @@
 # Kill Nickelback
 
-In this exercises, you're going to use a conditional statement inside a comprehension. Let's look at a basic example.
-
-```python
-nums = range(10)
-small_numbers = [num for num in nums if num < 6]
-# Only add numbers to the new list if the value is less than 6
-
-words = ['big', 'red', 'dog', 'ate', 'his', 'food']
-three_letters_words = [ word.title() for word in words if len(word) == 3 ]
-# len(stringVariable) is equivalent to stringVariable.length in JavaScript
-```
-
-## Setup
-
-```
-mkdir -p ~/workspace/python/exercises/sets && cd $_
-touch nickelback.py
-```
+# In this exercises, you're going to use a conditional statement inside a comprehension. Let's look at a basic example.
 
 ## Instructions
 
-1. Define a set that contains tuples. Each tuple should contain two strings:
-    1. The name of an artist
-    1. A song by that artist
+# 1. Define a set that contains tuples. Each tuple should contain two strings:
+#     1. The name of an artist
+#     1. A song by that artist
 
-    Make sure that some of the songs are from the band Nickelback. You can see a [list of their greatest hits](https://www.amazon.com/Best-Nickelback-1/dp/B00FFERTUK/) on Amazon.
-    ```
-    # Example set
-    songs = {
-        ('Nickelback', 'How You Remind Me'), 
-        ('Will.i.am', 'That Power'),
-        ('Miles Davis', 'Stella by Starlight'),
-        ('Nickelback', 'Animals')
-    }
-    ```
-2. Using a set comprehension, create a new set that contains all songs that were not performed by Nickelback.
+#     Make sure that some of the songs are from the band Nickelback. You can see a [list of their greatest hits](https://www.amazon.com/Best-Nickelback-1/dp/B00FFERTUK/) on Amazon.
+
+songs = {
+    ('Nickelback', 'How You Remind Me'), 
+    ('Will.i.am', 'That Power'),
+    ('Miles Davis', 'Stella by Starlight'),
+    ('Nickelback', 'Animals'),
+    ('Thelonious Monk', 'Round Midnight'),
+    ('Phish', 'You Enjoy Myself')
+}
+
+good_songs = [song for song in songs if song[0] != 'Nickelback']
+print(good_songs)
+
+# 1. Using a set comprehension, create a new set that contains all songs that were not performed by Nickelback.
